11be179c9831044805b42466fd8342189c59c7f1
Isabela Santos <isantos.ipereira@gmail.com>
2025-03-19 13:28:31 -0300

    primeira parte do projeto

---
+++ src/main/java/com/generation/farmacia/model/Categoria.java
@@ -0,0 +1,36 @@
+package com.generation.farmacia.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+
+@Entity
+@Table(name = "tb_categorias")
+public class Categoria {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@NotBlank(message = "o setor da categoria é obrigatório")
+	private String setor;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getSetor() {
+		return setor;
+	}
+
+	public void setSetor(String setor) {
+		this.setor = setor;
+	}
+}
---
+++ src/main/java/com/generation/farmacia/model/Produto.java
@@ -0,0 +1,75 @@
+package com.generation.farmacia.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name = "tb_produtos")
+public class Produto {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@NotBlank(message = "o nome do produto é obrigatório")
+	@Size(min = 3, max = 5000, message = "o nome precisa ser maior que 3 e menor que 5000 caractéres")
+	private String nome;
+	
+	@NotBlank(message = "a bula do produto é obrigatória")
+	@Size(min = 10, max = 10000, message = "a bula precisa ser maior que 10 e menor que 10000 caractéres")
+	private String bula;
+	
+	@NotNull
+	private Double preco;
+	
+	@NotBlank(message = "o público do produto é obrigatório - infantil/ adulto/ todas")
+	@Size(min = 6, max = 12, message = "o formato do público precisa ser maior que 6 e menor que 12 caractéres - Infantil/ adulto/ todos")
+	private String publico;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getBula() {
+		return bula;
+	}
+
+	public void setBula(String bula) {
+		this.bula = bula;
+	}
+
+	public Double getPreco() {
+		return preco;
+	}
+
+	public void setPreco(Double preco) {
+		this.preco = preco;
+	}
+
+	public String getPublico() {
+		return publico;
+	}
+
+	public void setPublico(String publico) {
+		this.publico = publico;
+	}
+
+}
